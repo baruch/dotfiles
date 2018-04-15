344392eb639fa7a7d7c9d7a5bb0d6307f7f65d46
Baruch Even <baruch@ev-en.org>
2018-04-15 12:49:38 +0300

    i3: add notify before lock

--- .local/bin/install_setup.sh
+++ .local/bin/install_setup.sh
@@ -5,7 +5,7 @@ if [ $(whoami) == "root" ]; then
         SUDO=""
 fi
 
-I3WM="i3 i3status i3lock i3lock-fancy imagemagick network-manager network-manager-gnome blueman gxkb x11-xserver-utils arandr unclutter redshift compton xautolock"
+I3WM="i3 i3status i3lock i3lock-fancy imagemagick network-manager network-manager-gnome blueman gxkb x11-xserver-utils arandr unclutter redshift compton xautolock xfce4-notifyd libnotify-bin"
 DEV="screen vim-gtk3 universal-ctags gcc cmake make clusterssh clang gdb"
 WEB="firefox-esr chromium vlc mplayer"
 MISC="tcpdump netcat-openbsd ntpdate etckeeper hdparm sdparm lsscsi nvme-cli pciutils ipmitool bpfcc-tools sg3-utils sudo"
