0d629408e659a124dc4b4c8933c1702cd1a90123
Baruch Even <baruch@ev-en.org>
2018-04-15 12:33:17 +0300

    i3: auto lock after 15 minutes

--- .local/bin/install_setup.sh
+++ .local/bin/install_setup.sh
@@ -5,5 +5,11 @@ if [ $(whoami) == "root" ]; then
         SUDO=""
 fi
 
+I3WM="i3 i3status i3lock i3lock-fancy imagemagick network-manager network-manager-gnome blueman gxkb x11-xserver-utils arandr unclutter redshift compton xautolock"
+DEV="screen vim-gtk3 universal-ctags gcc cmake make clusterssh clang gdb"
+WEB="firefox-esr chromium vlc mplayer"
+MISC="tcpdump netcat-openbsd ntpdate etckeeper hdparm sdparm lsscsi nvme-cli pciutils ipmitool bpfcc-tools sg3-utils sudo"
+APT="apt-file"
+
 $SUDO apt-get update
-$SUDO apt-get install redshift compton i3 i3status unclutter network-manager blueman gxkb x11-xserver-utils arandr screen vim-gtk3 universal-ctags gcc cmake network-manager network-manager-gnome
+$SUDO apt-get install ${I3WM} ${DEV} ${WEB} ${MISC} ${APT}
