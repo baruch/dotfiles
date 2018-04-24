c1f74c3f01e9b10139d6bd03c20f2a7e70eb0767
Baruch Even <baruch@ev-en.org>
2018-04-24 23:23:04 +0300

    install: add spotify to install

--- .local/bin/install_setup.sh
+++ .local/bin/install_setup.sh
@@ -10,6 +10,9 @@ DEV="screen vim-gtk3 universal-ctags gcc cmake make clusterssh clang gdb"
 WEB="firefox-esr chromium vlc mplayer"
 MISC="tcpdump netcat-openbsd ntpdate etckeeper hdparm sdparm lsscsi nvme-cli pciutils ipmitool bpfcc-tools sg3-utils sudo undistract-me"
 APT="apt-file"
+DESKTOP="spotify-client"
 
+$SUDO apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+echo deb http://repository.spotify.com stable non-free | $SUDO tee /etc/apt/sources.list.d/spotify.list
 $SUDO apt-get update
-$SUDO apt-get install ${I3WM} ${DEV} ${WEB} ${MISC} ${APT}
+$SUDO apt-get install ${I3WM} ${DEV} ${WEB} ${MISC} ${APT} ${DESKTOP}
